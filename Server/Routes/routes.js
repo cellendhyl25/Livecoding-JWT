3d2677326666d8216b914095bbe604b455c1b5a2
fenrisl <daniel.deutsch25@gmail.com>
2018-06-26 15:38:27 +0200

    part 1 signup no hashing

--- Server/Routes/routes.js
+++ Server/Routes/routes.js
@@ -1,10 +1,39 @@
 const express = require('express')
 const router = express.Router()
+const mysql = require('mysql')
 
+const connection = mysql.createConnection({
+
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'mydb'
+})
+
+connection.connect((err)=>{
+  if (err) throw err
+  console.log("connection to DB sucessful")}
+)
 
 router.get('/', (req, res) => {
     res.json('A SUCCESFUL FETCH on /')
-  })
+})
 
+router.post('/register', (req, res) => {
+    
+  const username = req.body.user.username
+  const password = req.body.user.password
 
-  module.exports = router
+  let sql = `INSERT INTO table1 (Username, Password) VALUES (?,?)`
+  const values = [
+    username, password
+  ]
+  connection.query(sql, values, (err, result) => 
+{
+  if (err) throw err;
+  console.log('database updated')
+})
+    res.send('end')
+})
+
+module.exports = router
---
+++ client/src/components/SignUp.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+
+class SignUP extends Component {
+
+    state = {
+        username: '',
+        password: ''
+    }
+
+    UpdateField = (event) => {this.setState({[event.target.name]: event.target.value})}
+
+    Submit = (event) => {
+
+        const user = this.state
+        axios.post(`http://localhost:3030/register`, {user})
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" name="username" placeholder="username" onChange={this.UpdateField}/>
+                    <input type="password" name="password" placeholder="password" onChange={this.UpdateField}/>
+                </form>
+                <button type="button" onClick={this.Submit}>Confirmer</button>
+            </div>
+        );
+    }
+}
+
+export default SignUP;
