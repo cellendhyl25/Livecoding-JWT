3a225ae708904ca1790b65dfaf5e17936ad445fd
fenrisl <daniel.deutsch25@gmail.com>
2018-06-27 11:03:48 +0200

    encrypt password

--- Server/Routes/routes.js
+++ Server/Routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const mysql = require('mysql')
+const bcrypt = require('bcrypt')
 
 const connection = mysql.createConnection({
 
@@ -20,20 +21,57 @@ router.get('/', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-    
+
   const username = req.body.user.username
   const password = req.body.user.password
 
-  let sql = `INSERT INTO table1 (Username, Password) VALUES (?,?)`
-  const values = [
-    username, password
-  ]
-  connection.query(sql, values, (err, result) => 
-{
-  if (err) throw err;
-  console.log('database updated')
-})
-    res.send('end')
-})
+
+bcrypt.hash(password, 16).then(hash => {
+        
+      let sql = `INSERT INTO table1 (Username, Password) VALUES (?,?)`
+        const values = [
+          username, hash
+        ]
+        connection.query(sql, values, (err, result) => 
+      {
+        if (err) throw err;
+        console.log('database updated')
+        res.send(result)
+      })
+    });
+});
+
+
+
+    
+//   const username = req.body.user.username
+//   const password = req.body.user.password
+
+//   let sql = `INSERT INTO table1 (Username, Password) VALUES (?,?)`
+//   const values = [
+//     username, password
+//   ]
+//   connection.query(sql, values, (err, result) => 
+// {
+//   if (err) throw err;
+//   console.log('database updated')
+// })
+//     res.send('end')
+// })
 
 module.exports = router
+
+
+
+// bcrypt
+// .hash(req.body.user.password, 16)
+// .then(hash => {
+//     let sql = `INSERT INTO table1 (Username, Password) VALUES ('${req.body.user.email}', '${hash}')`
+//     connection.query(sql, function (err, result) {
+//         if (err) 
+//             throw err;
+//         console.log("1 record inserted");
+//         res.send(JSON.stringify(result));
+//     });
+// })
+// })
